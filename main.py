8531b6634386e32dcf5e0f2cc502100cb4d35f76
NathanHoekstra <nathanhoekstra@hotmail.com>
2020-12-03 16:12:26 +0100

    Made the digital clock functional, added a handler for the generate invite button

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import sys
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore
 
 from ui.MainWindow import Ui_MainWindow
+from library.api_handler import ApiHandler
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
+
+        # Create timer for digital clock
+        self.clockTimer = QtCore.QTimer(self)
+        self.clockTimer.timeout.connect(self.showTime)
+        self.clockTimer.start(1000)
+
+        # Set handler for generate invite button
+        self.generateinvite.clicked.connect(self.onGenerateInviteClicked)
+
+    def showTime(self):
+        time = QtCore.QTime.currentTime()
+        self.lcdNumber.display(time.toString("hh:mm"))
+
+    def onGenerateInviteClicked(self):
+        print("Clicked on generate invite")
+        first_name = self.nameinput.text()
+        middle_name = self.middlenameinput.text()
+        last_name = self.lastnameinput.text()
+        print(f"The following input was given: {first_name} {middle_name} {last_name}.")
 
 
 app = QtWidgets.QApplication(sys.argv)
