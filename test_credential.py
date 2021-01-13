867c66bb4a37a1668fca00f7fe14c38ba7821cd5
NathanHoekstra <nathanhoekstra@hotmail.com>
2021-01-13 14:22:56 +0100

    Test_credential now works: Giving yourself a credential

--- test_credential.py
+++ test_credential.py
@@ -5,7 +5,8 @@ import time
 if __name__ == "__main__":
     # Create handler instances for both mobile and desktop
     mobile = ApiHandler("localhost", 7003)
-    desktop = ApiHandler("localhost", 7001)
+    # --- Test to issue credential to itself, variable names are not correct! ---
+    desktop = ApiHandler("localhost", 7003)
 
     # Create a auto-accept invitation on the mobile ACA-PY
     desktop_conn_id, invitation = desktop.create_invitation(
@@ -39,177 +40,66 @@ if __name__ == "__main__":
     print("-" * 50)
 
     # Create schema on mobile
-    schema = mobile.create_schema(naw)
+    schema = mobile.create_schema(schema=naw)
     # Create credential definition on mobile
-    cred_def_id = mobile.create_credential_definition(schema["id"], naw["schema_name"], support_revocation=False)
+    cred_def_id = mobile.create_credential_definition(
+        schema_id=schema["id"],
+        schema_tag=naw["schema_name"],
+        support_revocation=False
+    )
+
+    time.sleep(4)
 
     # Issue a NAW credential to desktop
     response = mobile.issue_credential(
         conn_id=mobile_conn_id,
         cred_def_id=cred_def_id,
         attributes=[
-            {
-                "name": "naam",
-                "value": "Pietje"
-            },
-            {
-                "name": "voorletters",
-                "value": ""
-            },
-            {
-                "name": "achternaam",
-                "value": "Puk"
-            },
-            {
-                "name": "geslacht",
-                "value": ""
-            },
-            {
-                "name": "geboortedatum",
-                "value": ""
-            },
-            {
-                "name": "geboorteland",
-                "value": ""
-            },
-            {
-                "name": "bsn",
-                "value": ""
-            },
-            {
-                "name": "geldigheid_id",
-                "value": ""
-            },
-            {
-                "name": "burgelijke_staat",
-                "value": ""
-            },
-            {
-                "name": "telefoonnummer",
-                "value": ""
-            },
-            {
-                "name": "email-adres",
-                "value": ""
-            },
-            {
-                "name": "straat",
-                "value": ""
-            },
-            {
-                "name": "huisnummer",
-                "value": ""
-            },
-            {
-                "name": "toevoeging",
-                "value": ""
-            },
-            {
-                "name": "postcode",
-                "value": ""
-            },
-            {
-                "name": "woonplaats",
-                "value": ""
-            },
-            {
-                "name": "provincie",
-                "value": ""
-            },
-            {
-                "name": "land",
-                "value": ""
-            },
-            {
-                "name": "polisnummer",
-                "value": ""
-            },
-            {
-                "name": "verzekeraar",
-                "value": ""
-            },
-            {
-                "name": "voornaam_mantelzorger",
-                "value": ""
-            },
-            {
-                "name": "tussenvoegsel_mantelzorger",
-                "value": ""
-            },
-            {
-                "name": "achternaam_mantelzorger",
-                "value": ""
-            },
-            {
-                "name": "telefoonnummer_mantelzorger",
-                "value": ""
-            },
-            {
-                "name": "mobielnummer_mantelzorger",
-                "value": ""
-            },
-            {
-                "name": "email-adres_mantelzorger",
-                "value": ""
-            },
-            {
-                "name": "voornaam_contactpersoon",
-                "value": ""
-            },
-            {
-                "name": "tussenvoegsel_contactpersoon",
-                "value": ""
-            },
-            {
-                "name": "achternaam_contactpersoon",
-                "value": ""
-            },
-            {
-                "name": "telefoonnummer_contactpersoon",
-                "value": ""
-            },
-            {
-                "name": "mobielnummer_contactpersoon",
-                "value": ""
-            },
-            {
-                "name": "email-adres_contactpersoon",
-                "value": ""
-            },
-            {
-                "name": "naam_huisarts",
-                "value": ""
-            },
-            {
-                "name": "straat_huisarts",
-                "value": ""
-            },
-            {
-                "name": "huisnnummer_huisarts",
-                "value": ""
-            },
-            {
-                "name": "toevoeging_huisarts",
-                "value": ""
-            },
-            {
-                "name": "telefoonnummer_huisarts",
-                "value": ""
-            },
-            {
-                "name": "agb-code_huisarts",
-                "value": ""
-            },
-            {
-                "name": "uzi-nummer_huisarts",
-                "value": ""
-            },
-            {
-                "name": "email-adres_huisarts",
-                "value": ""
-            }],
+            {"name": "naam", "value": "Pietje"},
+            {"name": "voorletters", "value": "a"},
+            {"name": "achternaam", "value": "Puk"},
+            {"name": "geslacht", "value": "a"},
+            {"name": "geboortedatum", "value": "Vandaag"},
+            {"name": "geboorteland", "value": "a"},
+            {"name": "bsn", "value": "a"},
+            {"name": "geldigheid_id", "value": "a"},
+            {"name": "burgelijke_staat", "value": "a"},
+            {"name": "telefoonnummer", "value": "a"},
+            {"name": "email-adres","value": "a"},
+            {"name": "straat", "value": "a"},
+            {"name": "huisnummer", "value": "a"},
+            {"name": "toevoeging", "value": "a"},
+            {"name": "postcode", "value": "a"},
+            {"name": "woonplaats", "value": "a"},
+            {"name": "provincie", "value": "a"},
+            {"name": "land", "value": "a"},
+            {"name": "polisnummer", "value": "a"},
+            {"name": "verzekeraar", "value": "a"},
+            {"name": "voornaam_mantelzorger", "value": "a"},
+            {"name": "tussenvoegsel_mantelzorger", "value": "a"},
+            {"name": "achternaam_mantelzorger", "value": "a"},
+            {"name": "telefoonnummer_mantelzorger", "value": "a"},
+            {"name": "mobielnummer_mantelzorger", "value": "a"},
+            {"name": "email-adres_mantelzorger", "value": "a"},
+            {"name": "voornaam_contactpersoon", "value": "a"},
+            {"name": "tussenvoegsel_contactpersoon", "value": "a"},
+            {"name": "achternaam_contactpersoon", "value": "a"},
+            {"name": "telefoonnummer_contactpersoon", "value": "a"},
+            {"name": "mobielnummer_contactpersoon", "value": "a"},
+            {"name": "email-adres_contactpersoon", "value": "a"},
+            {"name": "naam_huisarts", "value": "a"},
+            {"name": "straat_huisarts", "value": "a"},
+            {"name": "huisnnummer_huisarts", "value": "a"},
+            {"name": "toevoeging_huisarts", "value": "a"},
+            {"name": "telefoonnummer_huisarts", "value": ""},
+            {"name": "agb-code_huisarts", "value": ""},
+            {"name": "uzi-nummer_huisarts", "value": ""},
+            {"name": "email-adres_huisarts", "value": ""}],
         schema=schema)
     print(f"Issue cred response: {response}")
     time.sleep(3)
     # Show credential on desktop
-    print(desktop.get_credentials())
+    while not desktop.get_credentials()["results"]:
+        print("Credentials not received")
+        time.sleep(4)
+    print(f"Credentials received: {desktop.get_credentials()}")
